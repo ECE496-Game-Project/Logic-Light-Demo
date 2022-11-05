596d83969d5af134646b7cedbfaf851513a45b2d
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-11-05 18:18:54 -0400

    Move the majority herlper function in Light.cs to Utility.cs

--- Assets/Scripts/Light.cs
+++ Assets/Scripts/Light.cs
@@ -12,7 +12,7 @@ public class Light
 
     public const float MAX_DISTANCE = 30;
     public const int DETECTION_LAYER = 0;
-    public const int TEMPORARY_LAYER = 31;
+    
     public const int MAX_INTERACTION = 2;
     public const float FUDGE_FACTOR = 1e-4f;
 
@@ -82,7 +82,8 @@ public class Light
         //TO-DO: just use one stack
         Stack<GameObject> reflectionHier = hierarchy;
         Stack<GameObject> refractionHier = new Stack<GameObject>();
-        cloneStack<GameObject>(hierarchy, refractionHier);
+
+        Utility.cloneStack<GameObject>(hierarchy, refractionHier);
         
         int isHit = rayDetection(pos, dir, refractionHier, out hit, out n1, out n2);
         //Debug.Log(n1.ToString() + ", " + n2.ToString());
@@ -129,7 +130,7 @@ public class Light
         // Debug.Log(dir);
         //Debug.Log(hit.normal);
         
-        bool success = refract(dir, hit.normal, n1, n2, out refract_dir);
+        bool success = Utility.refract(dir, hit.normal, n1, n2, out refract_dir);
         // Debug.Log(refract_dir);
         if (success){
             newNode.refraction = castRayHelper(hit.point, refract_dir, refractionHier, numReflection, numRefraction + 1);
@@ -165,9 +166,9 @@ public class Light
             //Debug.Log("hit an object");
             //Debug.Log(hit.point);
             //check if it is in the current medium
-            if (isInside(hierarchy.Peek(), hit.point)){
-                n1 = getRefractiveIndex(hierarchy.Peek());
-                n2 = getRefractiveIndex(hit.collider.gameObject);
+            if (Utility.isInside(hierarchy.Peek(), hit.point)){
+                n1 = Utility.getRefractiveIndex(hierarchy.Peek());
+                n2 = Utility.getRefractiveIndex(hit.collider.gameObject);
                 hierarchy.Push(hit.collider.gameObject);
                 result++;
                 return result;
@@ -183,10 +184,10 @@ public class Light
 
         // put the current medium object into a special layer for faster
         // raycast detection
-        int oldLayer = switchObjLayer(current, TEMPORARY_LAYER);
-        mask =  1 << TEMPORARY_LAYER;
+        int oldLayer = Utility.switchObjLayer(current, Utility.TEMPORARY_LAYER);
+        mask =  1 << Utility.TEMPORARY_LAYER;
         bool isHit = Physics.Raycast(reversedRay, out hit, MAX_DISTANCE, mask);
-        switchObjLayer(current, oldLayer);
+        Utility.switchObjLayer(current, oldLayer);
 
         if (isHit){
             //Debug.Log(pos);
@@ -194,8 +195,8 @@ public class Light
             
             GameObject obj = hierarchy.Pop();
             result--;
-            n1 = getRefractiveIndex(obj);
-            n2 = (hierarchy.Count > 0)? getRefractiveIndex(hierarchy.Peek()):0;
+            n1 = Utility.getRefractiveIndex(obj);
+            n2 = (hierarchy.Count > 0)? Utility.getRefractiveIndex(hierarchy.Peek()):0;
             //Debug.Log(n1.ToString() + ", " + n2.ToString());
         }
         return result;
@@ -203,91 +204,11 @@ public class Light
         
     }
 
-    static int switchObjLayer(GameObject GO, int layer){
-        int old = GO.layer;
-        GO.layer = layer;
-        return old;
-    }
+    
 
     // might cause problem due to multi-threading
-    static bool isInside(GameObject GO, Vector3 pos){
-        int oldLayer = switchObjLayer(GO, TEMPORARY_LAYER);
-        LayerMask mask =  1 << TEMPORARY_LAYER;
-        Collider[] c = Physics.OverlapSphere(pos, 0f, mask);
-        switchObjLayer(GO, oldLayer);
-
-        switch (c.Length){
-            case 0:
-                return false;
-            case 1:
-                return true;
-            default:
-                Debug.Assert(false, "[ERROR]: more than 1 object in temporary layer");
-                return false;
-        }
-    }
-
-    bool refract(Vector3 inRay, Vector3 n, double n1, double n2, out Vector3 outRay){
-
-        Vector3 normal;
-        float thetaIn, thetaOut;
     
-        if (Vector3.Dot(-1 * inRay, n) < 0){
-            normal = -1 * n;
-        }else{
-            normal = n;
-        }
-
-        if (onlyReflection(inRay, normal, n1, n2)){
-            outRay = Vector3.zero;
-            //Debug.Log("return true");
-            return false;
-        }
-
-        //Debug.Log("return false");
-
-        thetaIn = Mathf.Acos(Vector3.Dot(-1 * inRay, normal));
-        thetaOut = Mathf.Asin((float)(n1/n2 * Mathf.Sin(thetaIn)));
-        //Debug.Log(thetaOut);
-
-        outRay = (float)(n1 / n2) * inRay + (float)(n1 / n2) * Mathf.Cos(thetaIn) * normal - Mathf.Cos(thetaOut) * normal;
-        outRay = Vector3.Normalize(outRay);
-        return true;
-        
-    }
     
-    static bool onlyReflection(Vector3 inRay, Vector3 normal, double n1, double n2){
-        if (n1 <= n2) {
-            //Debug.Log(n1.ToString() + ", " + n2.ToString());
-            return false;
-        }
-
-        float criticalAngle = Mathf.Asin((float)(n2/n1));
-        float thetaIn = Mathf.Acos(Vector3.Dot(-1 * inRay, normal));
-        //Debug.Log(criticalAngle.ToString() + ", " + thetaIn.ToString());
-
-        if (thetaIn <= criticalAngle){
-            //Debug.Log(criticalAngle.ToString() + ", " + thetaIn.ToString());
-            return false;
-        }
-        else{
-            return true;
-        } 
-    }
-
-    static double getRefractiveIndex(GameObject obj){
-        Medium m = obj.GetComponent<Medium>();
-        return m.refractiveIndex;
-    }
-
-    static void cloneStack<myType>(Stack<myType> from, Stack<myType> to){
-        myType[] temp = new myType[from.Count];
-        from.CopyTo(temp, 0);
-
-        for (int i = temp.Length - 1; i >= 0; i--){
-                to.Push(temp[i]);
-        }
-    }
  
     public static List<Tuple<Collider, Vector3>> crateTupleList(Collider[] colliders, Vector3 referencePoint){
         List<Tuple<Collider, Vector3>> result = new List<Tuple<Collider, Vector3>>();
@@ -342,12 +263,5 @@ public class Light
         return result;
     }
 
-    public static void printStack(Stack<GameObject> stack){
-        string output = "";
-        foreach(GameObject obj in stack){
-            output += obj.name + " ";
-        }
-
-        Debug.Log(output);
-    }
+    
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public const int TEMPORARY_LAYER = 31;
+    public static void cloneStack<myType>(Stack<myType> from, Stack<myType> to){
+        myType[] temp = new myType[from.Count];
+        from.CopyTo(temp, 0);
+
+        for (int i = temp.Length - 1; i >= 0; i--){
+                to.Push(temp[i]);
+        }
+    }
+
+    public static int switchObjLayer(GameObject GO, int layer){
+        int old = GO.layer;
+        GO.layer = layer;
+        return old;
+    }
+
+    public static bool refract(Vector3 inRay, Vector3 n, double n1, double n2, out Vector3 outRay){
+
+        Vector3 normal;
+        float thetaIn, thetaOut;
+    
+        if (Vector3.Dot(-1 * inRay, n) < 0){
+            normal = -1 * n;
+        }else{
+            normal = n;
+        }
+
+        if (onlyReflection(inRay, normal, n1, n2)){
+            outRay = Vector3.zero;
+            //Debug.Log("return true");
+            return false;
+        }
+
+        //Debug.Log("return false");
+
+        thetaIn = Mathf.Acos(Vector3.Dot(-1 * inRay, normal));
+        thetaOut = Mathf.Asin((float)(n1/n2 * Mathf.Sin(thetaIn)));
+        //Debug.Log(thetaOut);
+
+        outRay = (float)(n1 / n2) * inRay + (float)(n1 / n2) * Mathf.Cos(thetaIn) * normal - Mathf.Cos(thetaOut) * normal;
+        outRay = Vector3.Normalize(outRay);
+        return true;
+        
+    }
+
+    public static bool onlyReflection(Vector3 inRay, Vector3 normal, double n1, double n2){
+        if (n1 <= n2) {
+            //Debug.Log(n1.ToString() + ", " + n2.ToString());
+            return false;
+        }
+
+        float criticalAngle = Mathf.Asin((float)(n2/n1));
+        float thetaIn = Mathf.Acos(Vector3.Dot(-1 * inRay, normal));
+        //Debug.Log(criticalAngle.ToString() + ", " + thetaIn.ToString());
+
+        if (thetaIn <= criticalAngle){
+            //Debug.Log(criticalAngle.ToString() + ", " + thetaIn.ToString());
+            return false;
+        }
+        else{
+            return true;
+        } 
+    }
+
+    public static void printStack(Stack<GameObject> stack){
+        string output = "";
+        foreach(GameObject obj in stack){
+            output += obj.name + " ";
+        }
+
+        Debug.Log(output);
+    }
+
+    public static double getRefractiveIndex(GameObject obj){
+        Medium m = obj.GetComponent<Medium>();
+        return m.refractiveIndex;
+    }
+
+    public static bool isInside(GameObject GO, Vector3 pos){
+        int oldLayer = Utility.switchObjLayer(GO, TEMPORARY_LAYER);
+        LayerMask mask =  1 << TEMPORARY_LAYER;
+        Collider[] c = Physics.OverlapSphere(pos, 0f, mask);
+        Utility.switchObjLayer(GO, oldLayer);
+
+        switch (c.Length){
+            case 0:
+                return false;
+            case 1:
+                return true;
+            default:
+                Debug.Assert(false, "[ERROR]: more than 1 object in temporary layer");
+                return false;
+        }
+    }
+
+    
+}
